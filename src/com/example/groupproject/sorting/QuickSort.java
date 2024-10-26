ae2eb00d349d4c47d91de4e88c0f014964d25930
Dimas409 <82093956+Dimas409@users.noreply.github.com>
2024-10-26 17:37:57 +0300

    Feat: Добавлены классы QuickSort, BinarySearch, а также интерфейс Strategy как реализация паттерна стратегия

---
+++ src/com/example/groupproject/sorting/QuickSort.java
@@ -0,0 +1,43 @@
+package src.com.example.groupproject.sorting;
+
+import java.util.Comparator;
+
+public class QuickSort<T> implements Strategy<T> {
+    private Comparator<T> comparator;
+
+    public QuickSort(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
+
+    @Override
+    public void sort(T[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(T[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private int partition(T[] array, int low, int high) {
+        T pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (comparator.compare(array[j], pivot) < 0) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+    private void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
